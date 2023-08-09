6e80d1b8304d6987e15540e0634a7f51d50b044f
Sanjib <46724644+sanjibsau@users.noreply.github.com>
2023-08-09 18:20:09 +0530

    Update nodemailer.jsx

--- config/nodemailer.jsx
+++ config/nodemailer.jsx
@@ -10,8 +10,8 @@ export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-    user: 'sanjibsau79@gmail.com',
-    pass: 'dfwnvulpksamvnsq'
+    user: 'email address',
+    pass: 'password'
   }
 });
 
